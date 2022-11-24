91931badac868bb15747e99c7149f94cd4f5fc62
moosburger <moosburger@freenet.de>
2022-11-24 13:02:42 +0100

    Patches aktualisiert

--- Patches/n-asb-2021-08.sh
+++ Patches/n-asb-2021-08.sh
@@ -4,3 +4,15 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo  - n-asb-2021-08
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
+
+cd external/sqlite
+git fetch https://github.com/LineageOS/android_external_sqlite refs/changes/66/314466/1 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_external_sqlite refs/changes/67/314467/3 && git cherry-pick FETCH_HEAD
+cd ../..
+cd packages/apps/Settings
+git fetch https://github.com/LineageOS/android_packages_apps_Settings refs/changes/68/314468/2 && git cherry-pick FETCH_HEAD
+git fetch https://github.com/LineageOS/android_packages_apps_Settings refs/changes/69/314469/2 && git cherry-pick FETCH_HEAD
+cd ../../..
+cd packages/services/Telephony
+git fetch https://github.com/LineageOS/android_packages_services_Telephony refs/changes/70/314470/1 && git cherry-pick FETCH_HEAD
+cd ../../..
--- Patches/n-asb-2022-10.sh
+++ Patches/n-asb-2022-10.sh
@@ -4,3 +4,10 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 echo  - n-asb-2022-10
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
+
+cd system/bt
+git fetch https://github.com/LineageOS/android_system_bt refs/changes/70/341070/1 && git cherry-pick FETCH_HEAD
+cd ../..
+cd external/libnfc-nci
+git fetch https://github.com/LineageOS/android_external_libnfc-nci refs/changes/71/341071/1 && git cherry-pick FETCH_HEAD
+cd ../..
